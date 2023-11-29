a5a403185b0f4c2a5a1466501ed5b85e2d64f9e5
SElshal1 <selshals508@gmail.com>
2023-11-29 07:41:57 -0800

    0x07. Python - Test-driven development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-"""
-Defines a function for integer addition
-"""
+"""Defines an integer addition function."""
 
 
 def add_integer(a, b=98):
-    """to printinteger addition of teo numbers
+    """Return the integer addition of a and b.
 
-    Args:
-        a: (int/float)the first number
-        b: (int/float)the second number pre-set to 98
+    Float arguments are typecasted to ints before addition is performed.
 
     Raises:
-        TypeError if a and b are not int or float
-
-    Returns:
-        the integer addition of a and b
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    if (not(isinstance(a, int)
-            or isinstance(a, float))):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if (not(isinstance(b, int) or
-            isinstance(b, float))):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
