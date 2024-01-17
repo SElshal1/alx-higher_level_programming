8169948bddd654c01dfd1253da72e5b2e9b89153
SElshal1 <73969852+SElshal1@users.noreply.github.com>
2024-01-17 21:33:28 +0200

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,4 @@
+-- CREATE FIL TB second_table ATT id, name .
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
