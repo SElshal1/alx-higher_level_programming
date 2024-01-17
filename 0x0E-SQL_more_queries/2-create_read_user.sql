4ad0076babe9b2e3c88d621f279ba509845efd12
SElshal1 <73969852+SElshal1@users.noreply.github.com>
2024-01-17 22:07:28 +0200

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- CREATE db hbtn , user 
 -- CREATE db
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- creates a user
+-- CREATE db a user
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
--- grants SELECT privileges to a user
+-- granT SELECT priv'S to user
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
