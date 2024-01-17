4c42cc6544afc5ab9f984994cdf0b904c3ca60f5
SElshal1 <73969852+SElshal1@users.noreply.github.com>
2024-01-17 21:53:30 +0200

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
--- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
--- lists all rows of a database that have one column in common
+-- list contain hbtn
+-- list row db one column 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
