28aab7628e78a16475701941f72dba8693d5f0f8
SElshal1 <selshals508@gmail.com>
2024-03-02 16:39:01 -0800

    0x10-python-network_0

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Check if the URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a GET request with curl, including the X-School-User-Id header
-curl -s -H "X-School-User-Id: 98" "$1"
+# Bash script that takes in a URL as an argument
+curl "$1" -sX GET -H "X-School-User-Id: 98"
