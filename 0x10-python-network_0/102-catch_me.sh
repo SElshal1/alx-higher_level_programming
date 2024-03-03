ba16ac83295dd657bff47b6e51e8377497118e85
SElshal1 <selshals508@gmail.com>
2024-03-02 16:45:46 -0800

    0x10-python-network_0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-# Send a POST request with curl to 0.0.0.0:5000/catch_me
-curl -s -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+# Bash script that takes in a URL as an argument
+curl "$1" -sX GET -H "You got me!"
